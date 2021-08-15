f08dd6d7230493ebf99ac7ed4260c425c913cab2
Forgotten <forgottenrunes@protonmail.com>
2021-08-14 19:30:12 -0500

    wip on artifact modal

--- components/AddLore/ArtifactPicker.tsx
+++ components/AddLore/ArtifactPicker.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import { useState } from "react";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 import { EmptyWell } from "../ui/EmptyWell";
 import useWeb3Modal from "../../hooks/useWeb3Modal";
 import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import { ConnectWalletButton } from "../web3/ConnectWalletButton";
 import Button from "../ui/Button";
+import Modal from "react-modal";
+import { ModalDecorator } from "../ui/ModalDecorator";
+import StyledModal from "./StyledModal";
+
+const ArtifactPickerModalElement = styled.div``;
+
+function ArtifactPickerModal({ onRequestClose }: any) {
+  return (
+    <ArtifactPickerModalElement>
+      <h1>Pick an Artifact</h1>
+      <Button onClick={onRequestClose}>Done</Button>
+    </ArtifactPickerModalElement>
+  );
+}
 
 type Props = {};
 
@@ -20,12 +35,19 @@ const ArtifactPickerElement = styled.div``;
 
 export default function ArtifactPicker({}: Props) {
   const haveArtifact = false;
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const closeModal = () => setModalIsOpen(false);
 
   if (!haveArtifact) {
     return (
       <EmptyWell>
         <div>
-          <Button>Pick an Artifact NFT</Button>
+          <Button onClick={() => setModalIsOpen(!modalIsOpen)}>
+            Pick an Artifact NFT
+          </Button>
+          <StyledModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+            <ArtifactPickerModal onRequestClose={closeModal} />
+          </StyledModal>
         </div>
       </EmptyWell>
     );
