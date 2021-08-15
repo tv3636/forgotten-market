a88348dc5ec09e0edd32b0aa5105351afa71c555
Forgotten <forgottenrunes@protonmail.com>
2021-08-14 21:32:11 -0500

    added images for book of lore

--- components/NFTDisplay.tsx
+++ components/NFTDisplay.tsx
@@ -10,6 +10,12 @@ type Props = {
 const NFTDisplayElement = styled.div``;
 
 export default function NFTDisplay({ contractId, tokenId }: Props) {
+  // if you have a contractId and tokenId,
+  // then fetch the erc721 contract (todo what about eip1155?)
+  // then pull the tokenURI
+  // then pull the IPFS stuff from cloudflare
+  // the embed the image
+
   return (
     <NFTDisplayElement>
       Displaying: {contractId} {tokenId}
