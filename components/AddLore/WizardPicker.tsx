b86d1f978084acb38017cfb17557713a20dee7ef
tv3636 <tv3636@gmail.com>
2021-08-16 14:27:10 -0700

    Update wizard picker to use popup and store selected wizard

--- components/AddLore/WizardPicker.tsx
+++ components/AddLore/WizardPicker.tsx
@@ -8,16 +8,59 @@ import { ConnectWalletButton } from "../web3/ConnectWalletButton";
 import { useMst } from "../../store";
 import { observer } from "mobx-react-lite";
 import { getWizardsContract } from "../../contracts/ForgottenRunesWizardsCultContract";
+import Button from "../ui/Button";
+import Modal from "react-modal";
+import { ModalDecorator } from "../ui/ModalDecorator";
+import StyledModal from "./StyledModal";
+import {
+  FormField,
+  TextInput,
+  TextAreaAutosizeInput
+} from "../../components/ui/Inputs";
 import WizardDiv from "../WizardDiv";
+import WizardCard from "../WizardCard";
 import productionWizardData from "../../data/nfts-prod.json";
 
-type Props = {};
-
-const WizardPickerElement = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-`;
 const wizData = productionWizardData as { [wizardId: string]: any };
+
+const WizardPickerModalElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1em;
+  width: 100%;
+  height: 100%;
+`;
+
+const WizardPickerFormContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 100%;
+  padding: 1em 0em 1em 0em;
+  margin-bottom: 2em;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  padding: 1.5em 0;
+`;
+
+const NftDisplayContainer = styled.div`
+  width: 100%;
+  margin: 2em 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AdvancedContainer = styled.div`
+  width: 100%;
+`;
+const TextFieldLayout = styled.div`
+  margin: 0.5em 0;
+`;
 
 function NotConnected() {
   return (
@@ -52,14 +95,70 @@ function WizardList(props: any) {
   });
   
   return (
-    <WizardDiv wizards={wizards}/>
+    <WizardDiv wizards={wizards} onClick={props.onClick}/>
   );
 }
 
+function WizardPickerModal({ onRequestClose, onWizardPicked, injectedProvider }: any) {
+  // onChange, debounce, e
+  const [inputUrl, setInputUrl] = useState<string | null>(null);
+  const [inputError, setInputError] = useState<string | null>(null);
+  const [contractAddress, setContractAddress] = useState<string | null>(null);
+  const [tokenId, setTokenId] = useState<string | null>(null);
+  const [showAdvanced, setShowAdvanced] = useState(false);
+
+  useEffect(() => {
+    if (tokenId) {
+      console.log(tokenId);
+    }
+  }, [inputUrl]);
+
+  return (
+    <WizardPickerModalElement>
+      <h1>Pick a Wizard</h1>
+      <WizardPickerFormContainer>
+        <WizardList injectedProvider={injectedProvider} onClick={onWizardPicked}/>
+      </WizardPickerFormContainer>
+      <Button onClick={onRequestClose}>Done</Button>
+    </WizardPickerModalElement>
+  );
+}
+
+type Props = {
+  onWizardPicked: (WizardConfiguration: WizardConfiguration) => void;
+};
+
+const WizardPickerElement = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+/**
+ * Wizard Picker
+ *
+ * This component lets the user pick a Wizard to attach lore to.
+ *
+ **/
+
+export type WizardConfiguration = {
+  tokenId: string;
+  name: string;
+};
+
 const WizardPicker = observer(({}: Props) => {
   const { web3Settings } = useMst();
-
   const walletConnected = web3Settings.connected;
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const closeModal = () => setModalIsOpen(false);
+  const [currentWizard, setCurrentWizard] =
+    useState<WizardConfiguration | null>(null);
+
+  const onWizardModalPicked = (
+    WizardConfiguration: WizardConfiguration
+  ) => {
+    setCurrentWizard(WizardConfiguration);
+  };
 
   if (!walletConnected) {
     return (
@@ -69,14 +168,33 @@ const WizardPicker = observer(({}: Props) => {
     );
   }
 
-  const injectedProvider = web3Settings.injectedProvider;
-
-  if (injectedProvider) {
-    return <WizardPickerElement><WizardList injectedProvider={injectedProvider}/></WizardPickerElement>;
-  } else {
-    return <WizardPickerElement></WizardPickerElement>;
+  function wizardPicked(tokenId: string, name: string) {
+    onWizardModalPicked({tokenId, name});
   }
-  
+
+  return (
+    <WizardPickerElement>
+      <EmptyWell solid={currentWizard ? true : false}>
+        {currentWizard && (
+          <WizardCard
+            id={currentWizard.tokenId}
+            name={currentWizard.name}
+          />
+        )}
+
+          <Button onClick={() => setModalIsOpen(!modalIsOpen)}>
+            Pick {currentWizard ? "another" : "a"} Wizard
+          </Button>
+          <StyledModal isOpen={modalIsOpen} onRequestClose={closeModal}>
+            <WizardPickerModal
+              onRequestClose={closeModal}
+              onWizardPicked={wizardPicked}
+              injectedProvider={web3Settings.injectedProvider}
+            />
+          </StyledModal>
+      </EmptyWell>
+    </WizardPickerElement>
+  );
 });
 
 export default WizardPicker;
--- components/WizardDiv.tsx
+++ components/WizardDiv.tsx
@@ -1,25 +1,10 @@
 import styled from "@emotion/styled";
+import WizardCard from "./WizardCard";
 
 const image_base_url = 'https://nftz.forgottenrunes.com/wizards/alt/400-nobg/wizard-';
 const opensea_base_url = 'https://opensea.io/assets/0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42/';
 
 const WizardStyle = styled.div`
-  .wizard-image-div {
-      height: 171px;
-      width: 180.75px;
-      position: relative;
-      background-image: url("../static/img/frame-alt.png");
-      background-size: 180.75px 171px;
-      display: inline-block;
-      scroll-snap-align: end;
-      margin: 0.45%;
-  }
-
-  .wizard-image {
-      margin-left: 11%;
-      margin-top: 17%;
-  }
-
   .outer-div {
       background-color: #0e0e0e;
       top: 1.8em;
@@ -33,47 +18,20 @@ const WizardStyle = styled.div`
       flex-direction: row;
       flex-wrap: wrap;
   }
-
-  @font-face {
-      font-family: "Alagard";
-      src: url("/static/game/wizards/alagard.otf") format("opentype");
-    }
-
-  .name-div {
-      width: 107.25px;
-      height: 24.75px;
-      position: absolute;
-      left: 37.5px;
-      top: 1.5px;
-  }
-
-  .wizard-name {
-      text-align: center;
-      position: relative;
-      z-index: 1;
-      margin-top: 0.1em;
-      margin-left: 0.5em;
-      margin-right: 0.5em;
-      line-height: 1em;
-
-      color: #dfd1a8;
-      font-family: "Alagard";
-      font-size: 69%;
-  }
 `;
 
-function WizardCard(props: any) {
-    return (
-        <div className='wizard-image-div'>
-            <div className='name-div'><h3 className='wizard-name'>{props.name}</h3></div>
-            <a href={opensea_base_url + props.id}>
-                <img className='wizard-image' src={image_base_url + props.id + '.png'} height={131.25} width={131.25}/>
-            </a>
-        </div>
-    )
-}
-
 function WizardDiv(props: any) {
+  if (props.onClick) {
+    return (
+        <WizardStyle>
+            <div className='outer-div'>
+                {props.wizards.map((wizard: any) =>
+                    <WizardCard id={wizard.id} name={wizard.name} onClick={props.onClick}/>
+                )}
+            </div>
+        </WizardStyle>
+    )
+  } else {
     return (
         <WizardStyle>
             <div className='outer-div'>
@@ -83,6 +41,7 @@ function WizardDiv(props: any) {
             </div>
         </WizardStyle>
     )
+  }
 }
 
 export default WizardDiv;
