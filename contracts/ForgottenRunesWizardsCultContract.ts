21428d81c0e0ed08ff57f73eb5399fd82a39fc47
Forgotten <forgottenrunes@protonmail.com>
2021-08-18 11:16:43 -0500

    added model for book of lore contract

--- contracts/ForgottenRunesWizardsCultContract.ts
+++ contracts/ForgottenRunesWizardsCultContract.ts
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { abi as ForgottenRunesWizardsCultAbi } from "./ForgottenRunesWizardsCult.json";
+import { abi as BookOfLoreAbi } from "./BookOfLore.json";
 
 export function getWizardsContract({ provider }: { provider: any }) {
   if (!process.env.NEXT_PUBLIC_REACT_APP_WIZARDS_CONTRACT_ADDRESS) {
@@ -8,6 +9,19 @@ export function getWizardsContract({ provider }: { provider: any }) {
   return new ethers.Contract(
     process.env.NEXT_PUBLIC_REACT_APP_WIZARDS_CONTRACT_ADDRESS,
     ForgottenRunesWizardsCultAbi,
+    provider
+  );
+}
+
+const BOOK_OF_LORE_ADDRESS: { [chainId: number]: string } = {
+  4: `0xdEb9121865D634A15023C7724B38F5c7Db6C88bB`
+};
+
+export async function getBookOfLoreContract({ provider }: { provider: any }) {
+  const { chainId } = await provider.getNetwork();
+  return new ethers.Contract(
+    BOOK_OF_LORE_ADDRESS[chainId as number],
+    BookOfLoreAbi,
     provider
   );
 }
