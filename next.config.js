62928bb461ec0a0c247caffa8a2d9635a4d34aa0
nik <nik@saberr.com>
2021-08-21 09:31:31 +0100

    WizardCard (no border) for displaying wizards on gallery

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const root = path.resolve(__dirname);
 const withImages = require("next-images");
 const withSvgr = require("next-svgr");
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
+  extension: /\.mdx?$/,
 });
 
 // https://gist.github.com/diachedelic/6ded48f5c6442482fa69e91ec7ab1742
@@ -12,15 +12,14 @@ let nextConfig = {
   webpack: (config, options) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      react$: path.resolve(root, "./node_modules/react")
+      react$: path.resolve(root, "./node_modules/react"),
     };
     return config;
   },
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  // images: {
-  //   domains: ['nftz.forgottenrunes.com'],
-  // },
-
+  images: {
+    domains: ["nftz.forgottenrunes.com"],
+  },
 };
 
 nextConfig = withImages(nextConfig);
