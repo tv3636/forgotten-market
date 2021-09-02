f9f33f26ace44cb13c8ad8abf60c7072f2c0b09b
Forgotten <forgottenrunes@protonmail.com>
2021-09-02 17:46:57 -0500

    test data for book pageS

--- components/Lore/types.tsx
+++ components/Lore/types.tsx
@@ -3,11 +3,36 @@ export type ArtifactConfiguration = {
   tokenId: string;
 };
 
+export type LoreWizard = {
+  id: string;
+};
+
+// try to match the schema from bookoflore-subgraph
 export type Lore = {
+  id?: string;
+  index?: number;
   wizardId: string;
-  artifactConfiguration: ArtifactConfiguration;
+  assetAddress: string;
+  tokenId: string;
   isPixelArt?: boolean;
   title?: string;
   story?: string;
-  isNsfw?: boolean;
+  nsfw?: boolean;
+  strick?: boolean;
+  creator?: string;
+  parentLoreId?: number;
+  wizard: LoreWizard;
+};
+
+export type LorePage = {};
+
+export type WizardLores = {
+  wizardId: string;
+  lore: Lore[];
+};
+
+export type WizardLorePagination = {
+  previousWizardLore: WizardLores;
+  currentWizardLore: WizardLores;
+  nextWizardLore: WizardLores;
 };
