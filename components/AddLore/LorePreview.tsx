8313376e9f2a6669e873832cdc8c67ac39bb54b0
nik <nik@saberr.com>
2021-09-08 13:06:33 +0100

    Trying out a text color based on background

--- components/AddLore/LorePreview.tsx
+++ components/AddLore/LorePreview.tsx
@@ -84,7 +84,7 @@ const LoreStory = styled.div`
 
 function ParchmentPage({
   bgColor,
-  children
+  children,
 }: {
   bgColor: string | null;
   children: any;
@@ -126,15 +126,52 @@ const WizardNameHeader = styled.div`
   text-align: center;
 `;
 
+/*!
+ * Get the contrasting color for any hex color
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
+ * @param  {String} A hexcolor value
+ * @return {String} The contrasting color (black or white)
+ */
+function getContrast(hexcolor: string) {
+  // If a leading # is provided, remove it
+  if (hexcolor.slice(0, 1) === "#") {
+    hexcolor = hexcolor.slice(1);
+  }
+
+  // If a three-character hexcode, make six-character
+  if (hexcolor.length === 3) {
+    hexcolor = hexcolor
+      .split("")
+      .map(function (hex) {
+        return hex + hex;
+      })
+      .join("");
+  }
+
+  // Convert to RGB value
+  let r = parseInt(hexcolor.substr(0, 2), 16);
+  let g = parseInt(hexcolor.substr(2, 2), 16);
+  let b = parseInt(hexcolor.substr(4, 2), 16);
+
+  // Get YIQ ratio
+  let yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+  // Check contrast
+  return yiq >= 128 ? "black" : "white";
+}
+
 export default function LorePreview({
   currentArtifact,
   currentTitle,
   currentStory,
   currentBgColor,
-  currentWizard
+  currentWizard,
 }: Props) {
   const hasContent =
     currentTitle || currentStory || currentArtifact || currentWizard;
+
+  const textColor = getContrast(currentBgColor || "#000000");
 
   return (
     <LorePreviewElement>
@@ -151,7 +188,9 @@ export default function LorePreview({
               here
             </EmptyPreviewStyles>
           )}
-          {currentTitle && <LoreTitle>{currentTitle}</LoreTitle>}
+          {currentTitle && (
+            <LoreTitle style={{ color: textColor }}>{currentTitle}</LoreTitle>
+          )}
           {currentArtifact && (
             <NFTDisplay
               contractAddress={currentArtifact.contractAddress}
@@ -159,7 +198,7 @@ export default function LorePreview({
             />
           )}
           {currentStory && (
-            <LoreStory>
+            <LoreStory style={{ color: textColor }}>
               <LoreMarkdown>{currentStory}</LoreMarkdown>
             </LoreStory>
           )}
