095a676d31ee8a8a6217b55b8681e78d47a480b1
nik <nik@saberr.com>
2021-09-14 18:21:18 +0100

    Converting base64 images to actual images on IPFS etc

--- pages/api/lore.ts
+++ pages/api/lore.ts
@@ -3,6 +3,8 @@ import pinataSDK from "@pinata/sdk";
 import { utils } from "ethers";
 import { getProvider } from "../../hooks/useProvider";
 import { getWizardsContract } from "../../contracts/ForgottenRunesWizardsCultContract";
+import replaceAsync from "string-replace-async";
+import fs from "fs";
 
 const pinata = pinataSDK(
   process.env.PINATA_ID || "",
@@ -40,10 +42,52 @@ export default async function handler(
     });
   }
 
+  let storyWithUploadedImages;
+
+  if (req.body?.story) {
+    storyWithUploadedImages = await replaceAsync(
+      req.body?.story,
+      /!\[(.*?)\]\(((?:data).*?)\)/gim,
+      // @ts-ignore
+      async (whole: string, alt: string, base64image: string) => {
+        const splitBase64 = base64image.split(";base64,");
+        const dataPart = splitBase64[0];
+        console.log(dataPart);
+        const extension = dataPart.substr(dataPart.lastIndexOf("/") + 1);
+
+        const filename = `img_${req.body.wizard_id}.${extension}`;
+
+        fs.writeFileSync(filename, splitBase64[1], { encoding: "base64" });
+
+        try {
+          const res = await pinata.pinFileToIPFS(
+            fs.createReadStream(filename),
+            {
+              pinataMetadata: {
+                name: "Converted Base64 Image",
+              },
+              pinataOptions: {
+                wrapWithDirectory: true,
+              },
+            }
+          );
+          return `![${alt}](ipfs://${res.IpfsHash}/${filename})`; // leaving as ipfs://  so we can use as-is, can always replace on frontend to another provider
+        } catch (e) {
+          console.error(e);
+          return res.status(400).json({
+            error: `Problem extracting and uploading images to IPFS: ${e}.`,
+          });
+        }
+      }
+    );
+  }
+
   try {
     const response = await pinata.pinJSONToIPFS({
       name: req.body?.title,
-      description: req.body?.story,
+      description: storyWithUploadedImages,
+      // original_story: req.body?.story,
+      // ^ Should we save original in case? I guess not since editing is allowed to fix any issues etc
       background_color: req.body?.bg_color, // Note: needs to be without # for compliance with spec
       attributes: [
         { trait_type: "Artifact Address", value: req.body.address },
