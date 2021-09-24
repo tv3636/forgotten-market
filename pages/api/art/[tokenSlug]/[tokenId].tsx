0e44f1779c61e3fdbd6f9015b96531c3e280ac41
Forgotten <forgottenrunes@protonmail.com>
2021-09-24 10:49:36 -0500

    added in sizes

--- pages/api/art/[tokenSlug]/[tokenId].tsx
+++ pages/api/art/[tokenSlug]/[tokenId].tsx
@@ -47,90 +47,98 @@ export default async function handler(
     tokenId: tokenId as string,
   });
 
-  // single token buffer
-  let genOptions: GetStyledTokenBufferProps = {
-    tokenSlug: tokenSlug as string,
-    tokenId: tokenId as string,
-    width: widthOption,
-    trim: trimOption,
-  };
-  const singleTokenBuffer = await getStyledTokenBuffer(genOptions);
-
-  let zipFiles = [
-    [
-      bufferToStream(singleTokenBuffer),
-      { name: `${tokenId}-${slugify(tokenData.name)}.png` },
-    ],
-  ];
-
   if (isZip) {
+    const sizes = [50, 400, 1024];
+
+    let zipFiles = [];
+
+    for (let i = 0; i < sizes.length; i++) {
+      const size = sizes[i];
+
+      // single token buffer
+      let genOptions: GetStyledTokenBufferProps = {
+        tokenSlug: tokenSlug as string,
+        tokenId: tokenId as string,
+        width: size,
+        trim: trimOption,
+      };
+      const singleTokenBuffer = await getStyledTokenBuffer(genOptions);
+      zipFiles.push([
+        bufferToStream(singleTokenBuffer),
+        { name: `${size}/${tokenId}-${slugify(tokenData.name)}.png` },
+      ]);
+
+      // build layers
+      const tokenTraitLayerNames = tokenLayerNames[tokenSlug as string];
+      for (let j = 0; j < tokenTraitLayerNames.length; j++) {
+        const traitSlug = tokenTraitLayerNames[j];
+        const frameNum = await getTokenFrameNumber({
+          tokenSlug: tokenSlug as string,
+          tokenId,
+          traitSlug,
+        });
+        if (frameNum < 0 || frameNum == 7777) {
+          continue;
+        }
+
+        const layerDescription =
+          tokenTraitsByIndex[tokenSlug as string][frameNum.toString()];
+        const layerBuffer = await getTraitLayerBufferForTokenId({
+          tokenSlug: tokenSlug as string,
+          tokenId: tokenId as string,
+          width: size,
+          traitSlug: traitSlug,
+          trim: trimOption,
+        });
+        zipFiles.push([
+          bufferToStream(layerBuffer),
+          {
+            name: `${size}/${traitSlug}-${slugify(layerDescription.label)}.png`,
+          },
+        ]);
+      }
+
+      // build nobg
+      let noBgGenOptions: GetStyledTokenBufferProps = {
+        tokenSlug: tokenSlug as string,
+        tokenId: tokenId as string,
+        width: size,
+        trim: trimOption,
+        noBg: true,
+      };
+      const noBgTokenBuffer = await getStyledTokenBuffer(noBgGenOptions);
+
+      zipFiles.push([
+        bufferToStream(noBgTokenBuffer),
+        { name: `${size}/${tokenId}-${slugify(tokenData.name)}-nobg.png` },
+      ]);
+
+      // build sepia
+      let parchmentGenOptions: GetStyledTokenBufferProps = {
+        tokenSlug: tokenSlug as string,
+        tokenId: tokenId as string,
+        width: size,
+        trim: trimOption,
+        sepia: true,
+      };
+      const parchmentTokenBuffer = await getStyledTokenBuffer(
+        parchmentGenOptions
+      );
+      zipFiles.push([
+        bufferToStream(parchmentTokenBuffer),
+        { name: `${size}/${tokenId}-${slugify(tokenData.name)}-parchment.png` },
+      ]);
+    }
+
     // build readme
     const readmeText = await buildReadme({
       tokenSlug: tokenSlug as string,
       tokenId: tokenId as string,
     });
+    zipFiles.push([readmeText, { name: "README.md" }]);
 
-    // build layers
-    const tokenTraitLayerNames = tokenLayerNames[tokenSlug as string];
-    for (let i = 0; i < tokenTraitLayerNames.length; i++) {
-      const traitSlug = tokenTraitLayerNames[i];
-      const frameNum = await getTokenFrameNumber({
-        tokenSlug: tokenSlug as string,
-        tokenId,
-        traitSlug,
-      });
-      if (frameNum < 0 || frameNum == 7777) {
-        continue;
-      }
-
-      const layerDescription =
-        tokenTraitsByIndex[tokenSlug as string][frameNum.toString()];
-      const layerBuffer = await getTraitLayerBufferForTokenId({
-        tokenSlug: tokenSlug as string,
-        tokenId: tokenId as string,
-        width: widthOption,
-        traitSlug: traitSlug,
-        trim: trimOption,
-      });
-      zipFiles.push([
-        bufferToStream(layerBuffer),
-        { name: `${traitSlug}-${slugify(layerDescription.label)}.png` },
-      ]);
-    }
-
-    // build nobg
-    let noBgGenOptions: GetStyledTokenBufferProps = {
-      tokenSlug: tokenSlug as string,
-      tokenId: tokenId as string,
-      width: widthOption,
-      trim: trimOption,
-      noBg: true,
-    };
-    const noBgTokenBuffer = await getStyledTokenBuffer(noBgGenOptions);
-
-    zipFiles.push([
-      bufferToStream(noBgTokenBuffer),
-      { name: `${tokenId}-${slugify(tokenData.name)}-nobg.png` },
-    ]);
-
-    // build sepia
-    let parchmentGenOptions: GetStyledTokenBufferProps = {
-      tokenSlug: tokenSlug as string,
-      tokenId: tokenId as string,
-      width: widthOption,
-      trim: trimOption,
-      sepia: true,
-    };
-    const parchmentTokenBuffer = await getStyledTokenBuffer(
-      parchmentGenOptions
-    );
-    zipFiles.push([
-      bufferToStream(parchmentTokenBuffer),
-      { name: `${tokenId}-${slugify(tokenData.name)}-parchment.png` },
-    ]);
-
+    // Tell the browser that this is a zip file.
     res.writeHead(200, {
-      // Tell the browser that this is a zip file.
       "Content-Type": "application/zip",
       "Content-disposition": `attachment; filename=${tokenId}.zip`,
     });
@@ -138,13 +146,20 @@ export default async function handler(
     // create the zipfile
     const zip = archiver("zip");
     zip.pipe(res); // Pipe the zip file to the response
-    zip.append(readmeText, { name: "README.md" });
     forEach(zipFiles, ([buffer, opts]) => {
       zip.append(buffer as stream.Readable, opts as any);
     });
     zip.finalize();
   } else {
     // show the single Wizard token
+    let genOptions: GetStyledTokenBufferProps = {
+      tokenSlug: tokenSlug as string,
+      tokenId: tokenId as string,
+      width: widthOption,
+      trim: trimOption,
+    };
+    const singleTokenBuffer = await getStyledTokenBuffer(genOptions);
+
     const bufferStream = new stream.PassThrough();
     bufferStream.end(singleTokenBuffer);
     return bufferStream.pipe(res);
