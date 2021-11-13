642f15984297917a58550351a16c6f64e420af64
Forgotten <forgottenrunes@protonmail.com>
2021-11-13 10:00:29 -0600

    added cors for spritesheets

--- pages/api/art/[tokenSlug]/[tokenId]/spritesheet.png.tsx
+++ pages/api/art/[tokenSlug]/[tokenId]/spritesheet.png.tsx
@@ -12,11 +12,23 @@ import {
   stripExtension,
   VALID_TOKEN_STYLES,
 } from "../../../../../lib/art/artGeneration";
+import initMiddleware from "../../../../../lib/initMiddleware";
+import Cors from "cors";
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await cors(req, res);
+
   if (req.method !== "GET") {
     return res.status(404);
   }
