b42d17a5321c7fc109b787593178b3a38ff322b5
tv3636 <tv3636@gmail.com>
2022-05-31 15:44:23 -0700

    use new font for connection modal

--- components/Marketplace/MarketConnect.tsx
+++ components/Marketplace/MarketConnect.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useWeb3Modal from "../../hooks/useWeb3Modal";
-import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import Button from "../ui/Button";
 import { useMst } from "../../store";
 import { useEthers } from "@usedapp/core";
