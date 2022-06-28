a6b34fdeb2723bf3aee3d4f2a34ab5dbaf1a85d8
Bhavik Singh <bhaviksingh@gmail.com>
2022-06-28 13:09:36 -0700

    adjusting frame

--- components/Marketplace/TokenDisplay.tsx
+++ components/Marketplace/TokenDisplay.tsx
@@ -37,15 +37,19 @@ const ListingDisplay = styled.div`
 `;
 
 const NewFrame = styled.div`
-  width: 200px;
-  height: 295px;
+  --frameSize: 24px;
+
+  width: calc(100% + var(--frameSize));
+  height: calc(100% + 0.5 * var(--frameSize));
 
   position: absolute;
+  left: calc(-0.5 * var(--frameSize));
+  top: calc(-0.1 * var(--frameSize));
   z-index: 1;
   border-image-source: url(/static/img/newframe_black.png);
   border-image-slice: 30 35 30;
-  border-image-width: 34px;
-  border-image-outset: 5 12 10 12;
+  border-image-width: var(--frameSize);
+  border-image-outset: 0;
   border-style: solid;
 `;
 
